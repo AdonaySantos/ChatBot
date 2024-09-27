db6432af368bee3391d2f8b42a80feb5c7acd1b0
Adonay <adonay.rssantos@gmail.com>
2024-09-27 17:58:11 -0300

    Estrutura para gerar resposta pro perfil de usuário `admin`

--- src/chatbot/chatbot.service.js
+++ src/chatbot/chatbot.service.js
@@ -1,27 +1,35 @@
 const { myPhone } = require("../config/config");
-const client = require("./client.service/client")
-const admin = require("./admin.service/admin")
+const client = require("./client.service/client");
+const admin = require("./admin.service/admin");
 
 function setTypeOfResponse(user) {
-  // if (user.userPhoneNumber === myPhone) {
-    // Se o usuário for o administrador, chame adminResponse
-    // return admin.response(user);
-  // } else {
+  // Verifica se o usuário é o administrador
+  if (user.userPhoneNumber === myPhone) {
+    if (!admin.userContext[user]) {
+      admin.userContext[user] = { removingReservation: false }; // Inicializa o contexto do usuário
+    }
+
+    if (admin.userContext[user] && admin.userContext[user].removingReservation) {
+      return admin.removeReservation(user);
+    } else {
+      // Se o usuário for o administrador, chame adminResponse
+      return admin.response(user);
+    }
+  } else {
     // Se for um cliente, gerencie o contexto e responda apropriadamente
     if (!client.userContext[user]) {
       client.userContext[user] = { inBookingProcess: false }; // Inicializa o contexto do usuário
     }
 
     if (client.userContext[user].inBookingProcess) {
-      // Se o usere estiver no processo de agendamento
+      // Se o usuário estiver no processo de agendamento
       return client.handleBookingProcess(user);
     } else {
       return client.response(user);
     }
-
-  // }
+  }
 }
 
 module.exports = {
   setTypeOfResponse,
-}
+};
